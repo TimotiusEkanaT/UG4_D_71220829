599d947dd4844ea73d33662b1f992e0e862258cf
TimotiusEkanaT <117902158+TimotiusEkanaT@users.noreply.github.com>
2023-03-01 18:46:31 +0700

    Add files via upload

--- 3_D_71220829.py
+++ 3_D_71220829.py
@@ -1,5 +1,7 @@
+def cari_kata_terpanjang(kalimat):
+    kata_terpanjang = max(kalimat.split(" "))
+    return kata_terpanjang
 
 aiojgas = input("Masukkan sebuah kalimat: ")
-agrhqjtj = lambda nuashfu: max(nuashfu.split(" "))
-barbnetmj = agrhqjtj(aiojgas)
+barbnetmj = cari_kata_terpanjang(aiojgas)
 print("Kata terpanjang dalam kalimat adalah:", barbnetmj)
