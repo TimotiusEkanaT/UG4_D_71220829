44de9cd0669799fe270b0312e2f76c8be03bcdc2
TimotiusEkanaT <117902158+TimotiusEkanaT@users.noreply.github.com>
2023-03-01 19:14:14 +0700

    Add files via upload

---
+++ 4_D_71220829.py
@@ -0,0 +1,6 @@
+adfbga = input("Masukkan sekumpulan bilangan (pisahkan dengan koma): ")
+aih9fjqaef = [int(oiahsfa.strip()) for oiahsfa in adfbga.split(",")]
+dshgah = lambda oiahsfa: max(oiahsfa)
+print("Bilangan terbesar dari kumpulan bilangan yang di input adalah:", dshgah(aih9fjqaef))
+shzsfhfdh = lambda oiahsfa: min(oiahsfa)
+print("Bilangan terkecil dari kumpulan bilangan di input adalah:", shzsfhfdh(aih9fjqaef))
