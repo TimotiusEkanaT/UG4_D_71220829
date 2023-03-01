ed0071fc781a7ac7785cb7978f637feed972a1dd
TimotiusEkanaT <117902158+TimotiusEkanaT@users.noreply.github.com>
2023-03-01 18:38:49 +0700

    Add files via upload

---
+++ 3_D_71220829.py
@@ -0,0 +1,5 @@
+
+aiojgas = input("Masukkan sebuah kalimat: ")
+agrhqjtj = lambda nuashfu: max(nuashfu.split(" "))
+barbnetmj = agrhqjtj(aiojgas)
+print("Kata terpanjang dalam kalimat adalah:", barbnetmj)
